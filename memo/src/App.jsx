9e57b35d7da63b243c2fd6586079fda0d678a772
charles521 <354395702@qq.com>
2025-03-21 14:15:28 +0800

    memo init

---
+++ memo/src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from "./components/Hello";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hello />
+    </>
+  );
+}
+export default App;
