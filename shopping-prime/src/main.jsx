d45371a80c940f4cc258a05802b71693097db2c7
charles521 <354395702@qq.com>
2025-04-25 15:07:02 +0800

    emit css

--- shopping-prime/src/main.jsx
+++ shopping-prime/src/main.jsx
@@ -7,7 +7,6 @@ import App from "./App.jsx";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import "primereact/resources/primereact.css";
-
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
